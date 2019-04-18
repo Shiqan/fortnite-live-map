ecd194f20d779e3852017fe9c8ed4c3f488ac6df
Shiqan <ferron.saan@live.nl>
2019-04-18 19:39:37 +0200

    Add observers for fortnite-replay-watcher

---
+++ src/FortniteReplayObserver.FileMapTracker/TrackerFileObserver.cs
@@ -0,0 +1,64 @@
+﻿using FortniteMapTracker;
+using FortniteMapTracker.Core;
+using FortniteMapTracker.Core.Models;
+using FortniteMapTracker.ScreenCapture.Windows;
+using FortniteReplayObservers.File;
+using FortniteReplayReader.Core.Models;
+using Newtonsoft.Json;
+
+namespace FortniteReplayObserver.FileMapTracker
+{
+    public class TrackerFileObserver : BaseFileObserver<PlayerElimination>
+    {
+        protected override string CreateMessagePayload(PlayerElimination e)
+        {
+            if (e.Eliminated == Settings.UserName)
+            {
+                var payload = GetTrackerPayload();
+                payload.Status = TrackerStatus.Eliminated;
+                return JsonConvert.SerializeObject(payload);
+            }
+
+            if (e.Eliminator == Settings.UserName)
+            {
+                var payload = GetTrackerPayload();
+                payload.Status = TrackerStatus.Eliminator;
+                return JsonConvert.SerializeObject(payload);
+            }
+
+            return string.Empty;
+        }
+
+        private TrackerPayload GetTrackerPayload()
+        {
+            var world = "./images/world.jpg";
+            var tracker = new Tracker();
+
+            using (var minimap = new TempFile("jpeg"))
+            using (var screencapturer = new ScreenshotCapture())
+            {
+                screencapturer.TakeScreenshot(minimap.Path);
+                var coord = tracker.Match(minimap.Path, world);
+
+                return new TrackerPayload()
+                {
+                    Coord = coord
+                };
+            }
+        }
+
+        public class TrackerPayload
+        {
+            public Coord Coord { get; set; }
+            public TrackerStatus Status { get; set; }
+        }
+
+        public enum TrackerStatus
+        {
+            Update,
+            Eliminator,
+            Eliminated
+        }
+    }
+}
+
